01c8f4a1544bdc72d3018d3dd56a46887bac698f
KrzysztofGG <krzys.gora2@gmail.com>
2026-09-04 14:06:29 +0200

    cleanup

--- lab7 (first-angular-project)/src/app/friend-list/friend.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Friend } from './friend.model';
-
-describe('Friend', () => {
-  it('should create an instance', () => {
-    expect(new Friend("", "", "")).toBeTruthy();
-  });
-});
--- lab8/zadanie4/src/app/shopping-list/list-item.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ListItem } from './list-item.model';
-
-describe('ListItem', () => {
-  it('should create an instance', () => {
-    expect(new ListItem()).toBeTruthy();
-  });
-});
